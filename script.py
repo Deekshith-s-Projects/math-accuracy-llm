9bad57242b78cfc8eee02594ceadeca2ed33417b
Deekshith Rao <8099566+deekshith-rj@users.noreply.github.com>
2024-05-01 19:46:06 +0530

    1. Got the data; 2. Installed a few basic packages; 3. script.py added

---
+++ script.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+# df = pd.read_csv("Accuracy Calculation Dataset.csv")
+df = pd.read_excel("Accuracy Calculation Dataset.xlsx")  # CSV version has errors due to encoding issues
+
