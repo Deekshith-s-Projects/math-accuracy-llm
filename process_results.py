928da697be4fbcc0ff2b0efb88c096562001ca62
Deekshith Rao <8099566+deekshith-rj@users.noreply.github.com>
2024-05-05 14:46:26 +0530

    More cleanup

--- process_results.py
+++ process_results.py
@@ -2,19 +2,18 @@ import pandas as pd
 
 df = pd.read_pickle('Accuracy Calculation Dataset with LLM Evaluation Results.pkl')
 
-# Accuracy of the algorithm  # ~80% ACCURACY
+# Accuracy of the algorithm
 accuracy = df.loc[df['LLM Equivalence Evaluation (Response)'] != 
                   'NOT_APPLICABLE', 'llm_matches_human'].mean()
 print(f"Accuracy: {accuracy * 100:.2f}%")
 
 # Median response time captures how most users would experience the tool
-# 8.47s
 median_response_time = df.loc[
     df['LLM Equivalence Evaluation (Response)'] !='NOT_APPLICABLE', 
         'Time taken to complete the request'].median()
 print(f"Median response time: {median_response_time:.2f} seconds")
 
-# Average cost per request  # $0.017
+# Average cost per request
 avg_cost = df.loc[df['LLM Equivalence Evaluation (Response)'] !='NOT_APPLICABLE', 
                   'prompt_tokens'].mean() * 10/10**6 + \
     df.loc[df['LLM Equivalence Evaluation (Response)'] !='NOT_APPLICABLE', 
