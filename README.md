6e8683d02103b1a7217a00253133157aedaa83a5
Deekshith Rao <8099566+deekshith-rj@users.noreply.github.com>
2024-05-05 14:43:50 +0530

    1. main.py - moved the code to main(); 2. Cleaning up README and .xlsx data file

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ### Errors in the data
 
-Index=84 - Conversation History column - the last entry in the list in this string is incomplete, so got it from the same column where Index=85 and updated it in the CSV file.
+Index=84 - Conversation History - the last entry in the list in this string is incomplete, so got it from the row where Index=85 and updated it in the CSV file.
 
 ### Other Important Notes
 
--- main.py
+++ main.py
@@ -5,44 +5,51 @@ from langchain_openai import OpenAI, ChatOpenAI
 from utils import get_agent, evaluate_equivalence, prepare_data
 from pprint import pp
 
-load_dotenv()
 
-# llm = OpenAI(model='gpt-3.5-turbo-instruct', temperature=0)
-llm = ChatOpenAI(model='gpt-4-turbo', temperature=0)  
-# GPT-4-turbo shows significant improvement in accuracy over GPT-3.5-turbo.
+def main():
+    # llm = OpenAI(model='gpt-3.5-turbo-instruct', temperature=0)
+    llm = ChatOpenAI(model='gpt-4-turbo', temperature=0)  
+    # GPT-4-turbo shows significant improvement in accuracy over GPT-3.5-turbo.
 
-agent = get_agent(llm)
+    agent = get_agent(llm)
 
-df = prepare_data(pd.read_csv("Accuracy Calculation Dataset.csv"))
+    x = input()
+    
+    df = prepare_data(pd.read_csv("Accuracy Calculation Dataset.csv"))
 
-df[['llm_eval_results', 'Time taken to complete the request', 'prompt_tokens', 
-    'completion_tokens']] = df.apply(lambda x: evaluate_equivalence(
-        agent, x['last_question'], x['User Response']), axis=1, 
-        result_type ='expand')
-eval_map = {'Correct': 'EQUIVALENT', 'Incorrect': 'NOT_EQUIVALENT', 
-            'Insufficient context': 'NOT_APPLICABLE', 'Has error': 'HAS_ERROR'}
-df['LLM Equivalence Evaluation (Response)'] = \
-    df['llm_eval_results'].apply(lambda x: x['output']).map(eval_map)
-df['error'] = df['llm_eval_results'].apply(lambda x: x.get('error'))
-df['llm_matches_human'] = \
-    df['LLM Equivalence Evaluation (Response)'] == df['human_eval_corrected']
-# The column 'human_eval_corrected' is added to note the corrected version of 
-# 'Human Evaluation' column without messing with it. 
-# TODO: This correction needs manual effort though.
-
-
-# TODO: - LOW PRIORITY - Try using the whole history
-# Try using just the last question first, and use history only when there is  
-# insufficient context. This saves both time and money. 
-# Also, consider using caching for faster response times overall. 
-# ChatOpenAI() allows you to utilize memory to store previous conversation 
-# history. This helps 
+    df[['llm_eval_results', 'Time taken to complete the request', 'prompt_tokens', 
+        'completion_tokens']] = df.apply(lambda x: evaluate_equivalence(
+            agent, x['last_question'], x['User Response']), axis=1, 
+            result_type ='expand')
+    eval_map = {'Correct': 'EQUIVALENT', 'Incorrect': 'NOT_EQUIVALENT', 
+                'Insufficient context': 'NOT_APPLICABLE', 'Has error': 'HAS_ERROR'}
+    df['LLM Equivalence Evaluation (Response)'] = \
+        df['llm_eval_results'].apply(lambda x: x['output']).map(eval_map)
+    df['error'] = df['llm_eval_results'].apply(lambda x: x.get('error'))
+    df['llm_matches_human'] = \
+        df['LLM Equivalence Evaluation (Response)'] == df['human_eval_corrected']
+    # The column 'human_eval_corrected' is added to note the corrected version of 
+    # 'Human Evaluation' column without messing with it. 
+    # TODO: This correction needs manual effort though.
 
 
-# TODO: - LOW PRIORITY Try adding image link handling to the agent
+    # TODO: - LOW PRIORITY - Try using the whole history
+    # Try using just the last question first, and use history only when there is  
+    # insufficient context. This saves both time and money. 
+    # Also, consider using caching for faster response times overall. 
+    # ChatOpenAI() allows you to utilize memory to store previous conversation 
+    # history. This helps 
 
 
-# Saving the results
-df.iloc[:, :10].to_csv('Accuracy Calculation Dataset with LLM Evaluation Results.csv', 
-                       index=False)
-df.to_pickle('Accuracy Calculation Dataset with LLM Evaluation Results.pkl')
+    # TODO: - LOW PRIORITY Try adding image link handling to the agent
+
+
+    # Saving the results
+    df.iloc[:, :10].to_csv('Accuracy Calculation Dataset with LLM Evaluation Results.csv', 
+                        index=False)
+    df.to_pickle('Accuracy Calculation Dataset with LLM Evaluation Results.pkl')
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    main()
